2b40f401be540d766d94d00983dfd6a2b9b47716
juliana15paak <fvjulianaasilva976@gmail.com>
2025-11-25 18:35:57 -0300

    fix algorithm to find prime number

--- algorithms/findingprimenumber.py
+++ algorithms/findingprimenumber.py
@@ -2,20 +2,22 @@ from math import ceil, sqrt
 
 def FindPrimes(n):
   raiz = ceil(sqrt(n))
-  Primes = [n+1]
-  for i in range(2, n+1):
-    Primes[i] = True
+  if n<=2:
+    return []
+  Primes = [True] * n
+  Primes[0] = False
+  Primes[1] = False
   for i in range(2,raiz):
-    if Primes[i] == 1:
-      for i in range(2, i*j<=n):
-        Primes[i*j] = False
+    if Primes[i]:
+      for j in range(i*i, n, i):
+        Primes[j] = False
   return Primes
 
 
 n = int(input("Digite um número inteiro positivo: "))
 Primes = FindPrimes(n)
 print(f"Números primos até {n}:")
-for i in range(2, n+1):
+for i in range(2, n):
   if Primes[i]:
     print(i, end=" ")
 
